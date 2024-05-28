079a24ae81922bdcb1c62a497e8109526d496579
juandresabbat92 <juandresabbat92@gmail.com>
2024-05-28 17:43:54 +0200

    modify footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import React from 'react'; // Add this line
+import React from 'react';
+
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
@@ -6,5 +7,7 @@ const Footer = () => (
     </p>
   </div>
 );
+
 export default Footer;
+
 
